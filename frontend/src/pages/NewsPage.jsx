a1e435eb5e0b8b3f87ff576f7cef53c44dc0582e
Aditya <aditya24042011@gmail.com>
2026-03-20 19:43:04 +0530

    fix

--- frontend/src/pages/NewsPage.jsx
+++ frontend/src/pages/NewsPage.jsx
@@ -28,7 +28,7 @@ const NewsPage = () => {
 
   const fetchArticles = (url = null) => {
     const isInitial = !url;
-    let targetUrl = url || 'http://localhost:8001/api/articles/';
+    let targetUrl = url || 'http://13.62.76.2:8001/api/articles/';
 
     // Add filtering params to initial fetch
     if (isInitial) {
